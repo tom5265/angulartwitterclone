75b9079bd000cb9c7cb15f73a2b5a1bf1e354a2f
Tom Moore <tom5265@gmail.com>
2016-03-15 15:43:15 -0500

    more setting up

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,15 @@
-angular.module('myApp', [])
-	.controller('MyController', ['', function(){
-
-	}])
-	.config(['', function(){
-
-
+var app = angular.module('myApp', ['myApp.controllers', 'ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'HomePageController'
+    })
+    .when('/instructors', {
+        templateUrl: 'views/instructors.html',
+        controller: 'InstructorPageController'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
 }]);
