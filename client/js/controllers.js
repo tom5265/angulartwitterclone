35918dab519b949a1c102b0ea72a441432e536af
Tom Moore <tom5265@gmail.com>
2016-03-15 22:06:28 -0500

    beginning server functions

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,7 +1,6 @@
 var controllers = angular.module('myApp.controllers', []);
 
 controllers.controller('welcomeController', ['$scope', function($scope) {
-    $scope.hello = 'Hello World!';
-    $scope.weather = "It's sunny and 85 degrees outside!";
+   
 }]);
 
--- client/js/main.js
+++ client/js/main.js
@@ -1,15 +1,47 @@
 var app = angular.module('myApp', ['myApp.controllers', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
-    .when('/', {
-        templateUrl: 'views/welcome.html',
-        controller: 'welcomeController'
-    })
-    .when('/tweeter', {
-        templateUrl: 'views/tweets.html',
-        controller: 'tweeterController'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+        .when('/', {
+            templateUrl: 'views/welcome.html',
+            controller: 'welcomeController'
+        })
+        .when('/tweeter', {
+            templateUrl: 'views/tweets.html',
+            controller: 'tweeterController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
+
+function getData() {
+    $http({
+        method: 'GET',
+        url: 'messages',
+    }).then(function(data) {
+        var tweets = data.split('\n');
+        console.log(tweets);
+        for (var i = 0; i < tweets.length; i++) {
+            var tweet = JSON.parse(tweets[i]);
+            var twit = tweet.text;
+            var screenName = tweet.userName;
+            $('#tweet_list').prepend('<li>' + screenName + ': ' + twit + '</li>');
+        }
+    }).done(function() {
+        allTweets.push(tweet);
+        console.log(allTweets);
+    })
+}
+
+function postData(tweet, user) {
+        var message = {};
+        message.text = tweet;
+        message.userName = user;
+        $http({
+            method: 'POST',
+            url: 'messages',
+            data: JSON.stringify(message)
+        }).done(function() {
+            $('#tweet_list').prepend('<li>' + message.userName + ': ' + message.text + '</li>');
+            clearForm();
+        })}
