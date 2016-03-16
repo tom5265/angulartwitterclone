894561ddd0dffc92b047ffe23b4716f46782a827
Tom Moore <tom5265@gmail.com>
2016-03-16 11:18:41 -0500

    welcome username form working

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,15 +1,37 @@
 var controllers = angular.module('myApp.controllers', []);
 
-controllers.controller('welcomeController', ['$scope', function($scope) {
-
+controllers.controller('welcomeController', ['$scope','$location', function($scope, $location) {
+    $scope.userText;
+    $scope.loadTweeter = function (userText) {
+        if (userText == null || userText == '') {
+            alert('You must enter a username!');
+        }
+        else {
+            $location.path('/tweeter');
+        }
+    }
 }]);
 
 controllers.controller('tweeterController', ['$scope', '$http', function($scope, $http) {
     $http({
         method: 'GET',
         url: 'messages'
-    }).then(function(data) {
+    }).success(function(data) {
         $scope.tweets = data;
     }
-        )       
+        )
+    // $scope.postData = function (tweet, user) {
+    //     var message = {};
+    //     message.text = tweet;
+    //     message.userName = user;
+    //     $.http({
+    //         method: 'POST',
+    //         url: 'messages',
+    //     }).success(function(data) {
+    //         $scope.tweets = data;
+            
+    //     })
+    // }
+    
+
 }]);
