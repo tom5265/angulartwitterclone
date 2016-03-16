87bdcdf4c4b86b026cbb118a4f36709255b394b0
Tom Moore <tom5265@gmail.com>
2016-03-16 13:48:03 -0500

    username now stores

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,8 +1,11 @@
 var controllers = angular.module('myApp.controllers', []);
 
-controllers.controller('welcomeController', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope) {
+controllers.controller('welcomeController', ['$scope', '$location','$rootScope', function($scope, $location, $rootScope) {
     $scope.loadTweeter = function() {
-        if ($rootScope.userText == null || $rootScope.userText == '') {
+        var userLogName = $scope.userText;
+        $rootScope.userMan = userLogName;
+        
+        if ($scope.userText == null || $scope.userText == '') {
             alert('You must enter a username!');
         }
         else {
@@ -11,7 +14,7 @@ controllers.controller('welcomeController', ['$scope', '$location', '$rootScope'
     }
 }]);
 
-controllers.controller('tweeterController', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+controllers.controller('tweeterController', ['$scope', '$http','$rootScope', function($scope, $http, $rootScope) {
 
     $scope.getData = function() {
         $http({
@@ -26,7 +29,7 @@ controllers.controller('tweeterController', ['$scope', '$http', '$rootScope', fu
 
         var tweetToPost = {
             text: $scope.newTweet,
-            user: 'anonymous'
+            user: $rootScope.userMan
         };
         $http({
             method: 'POST',
